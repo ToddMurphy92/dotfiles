4da2845066d0f1b5136a8bb2d071fc77c338cd72
Todd Murphy <todd@toddbook.localdomain>
2023-07-11 15:14:35 +1000

    Added vimwiki

---
+++ nvim/.config/nvim/lua/custom/plugins/vimwiki.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "vimwiki/vimwiki",
+    -- init = function()
+    --   vim.g.vimwiki_list = {
+    --     { path = '~/st/pm/notes', syntax = 'markdown', ext = '.md', links_space_char = '-' },
+    --     { path = '~/st/pm/work_notes', syntax = 'markdown', ext = '.md', links_space_char = '-' },
+    --   }
+    --   vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown' }
+    --   vim.g.vimwiki_use_mouse = 1
+    --   vim.g.vimwiki_markdown_link_ext = 1
+    -- end,
+  },
+}
