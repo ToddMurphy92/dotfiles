d3200277a7335f38ec4eec04e763052d283c1c31
Todd Murphy <todd@toddbook.localdomain>
2023-08-06 20:52:52 +1000

    Added descriptions to Harpoon keymaps

--- nvim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -3,17 +3,16 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim'
   },
-  config = function ()
+  config = function()
     local mark = require('harpoon.mark')
     local ui = require('harpoon.ui')
 
-    vim.keymap.set('n', '<leader>ha', mark.add_file)
-    vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+    vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = "[H]arpoon - [A]dd file" })
+    vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, { desc = "[H]arpoon - [T]oggle quick menu" })
 
-    vim.keymap.set('n', '<C-h>', function() ui.nav_file(1) end)
-    vim.keymap.set('n', '<C-t>', function() ui.nav_file(2) end)
+    vim.keymap.set('n', '<C-h>', function() ui.nav_file(1) end, { desc = "[H]arpoon - First file" })
+    vim.keymap.set('n', '<C-t>', function() ui.nav_file(2) end, { desc = "[H]arpoon - Second file" })
     --vim.keymap.set('n', '<C-n>', function() ui.nav_file(3) end) -- This one is used by nvim-cmp
-    vim.keymap.set('n', '<C-s>', function() ui.nav_file(3) end)
-
+    vim.keymap.set('n', '<C-s>', function() ui.nav_file(3) end, { desc = "[H]arpoon - Third file" })
   end
 }
