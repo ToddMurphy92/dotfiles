45f2e05279f40d93751e563b407f81a98a69547d
Todd Murphy <admin@toddmurphy.me>
2024-10-11 15:22:00 +1000

    Moved around fish files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     # Enable atuin
     atuin init fish | source
-    oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json | source
+    # oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json | source
 end
