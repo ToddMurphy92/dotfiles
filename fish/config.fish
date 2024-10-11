6a0d1c6ddc672cfa37baf7957ddcd506d4fbb790
Todd Murphy <admin@toddmurphy.me>
2024-10-11 14:37:46 +1000

    Initial fish config commit

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    # Enable atuin
+    atuin init fish | source
+    oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json | source
+end
