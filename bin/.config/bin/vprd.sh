863b802d6a4e75426cdf18a6d2af39831bdf80d1
Todd Murphy <admin@toddmurphy.me>
2024-10-14 15:04:24 +1000

    Implemented a hack to improve how prompt shows the vault env

--- bin/.config/bin/vprd.sh
+++ bin/.config/bin/vprd.sh
@@ -3,4 +3,5 @@ set -Ux VAULT_ADDR https://vault.aws.superretailgroup.com/
 set -Ux VAULT_SSH_PATH ssh-key-signer
 set -Ux VAULT_SSH_PUBLIC_KEY ~/.ssh/id_ed25519.pub
 set -Ux VAULT_SSH_ROLE wildcard_modern_role
+set -Ux VAULT_ENV " PRD"
 echo "Finished setting up Vault PRD environment variables"
