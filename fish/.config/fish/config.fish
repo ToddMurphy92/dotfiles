f4e8c23e3f2fdbb71b93fd3779c39963d6339a41
Todd Murphy <admin@toddmurphy.me>
2024-10-15 08:21:55 +1000

    Removed VAULT_ENV

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,12 +4,4 @@ if status is-interactive
     atuin init fish | source
     # oh-my-posh init fish --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json | source
     oh-my-posh init fish --config ~/dotfiles/ohmyposh/catppuccin_mocha_devops.omp.json | source
-    # If $VAULT_ADDR = https://vault.aws.superretailgroup.com/ then set VAULT_ENV = PRD
-    if test $VAULT_ADDR = "https://vault.aws.superretailgroup.com/"
-        set -Ux VAULT_ENV " PRD"
-    else if test $VAULT_ADDR
-        set -Ux VAULT_ENV " OTHER"
-    else
-        set -e VAULT_ENV
-    end
 end
