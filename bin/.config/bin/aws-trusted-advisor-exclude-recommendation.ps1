1cf8081b7fbe69101df9ae9f6d2f61b533778601
Todd Murphy <admin@toddmurphy.me>
2025-04-04 13:22:38 +1000

    Script for excluding trusted advisor recommendations

---
+++ bin/.config/bin/aws-trusted-advisor-exclude-recommendation.ps1
@@ -0,0 +1,35 @@
+#!/usr/local/bin/pwsh
+#requires -Module AWS.Tools.TrustedAdvisor  
+
+param (
+  [string]$Name
+)
+
+$RecommendationList = Get-TARecommendationList | Where-Object { $_.Name -like "*$Name*" }
+# Example: 
+#$RecommendationList         = Get-TARecommendationList | ? { $_.Name -like "*public IPv4*"}  
+
+# Check the length of the recommendation list
+Write-Output "Length of Recommendation List: $($RecommendationList.Count)"
+
+# Print the recommendations list
+Write-Output "Recommendations Arn: $($RecommendationList.Arn)"
+
+
+$RecommendationResourceList = Get-TARecommendationResourceList -RecommendationIdentifier $RecommendationList.Arn | Where-Object { $_.Status -eq "Error" }  
+Write-Output "Recommendation Resource List: $($RecommendationResourceList.Count)"
+
+# Ask user if they are sure before proceeding
+Write-Host "Are you sure you want to exclude the following recommendations? (Y/N)"
+$response = Read-Host
+
+if ($response -ne 'Y' -and $response -ne 'y') {
+  Write-Output "Operation cancelled."
+  exit
+}
+
+$RecommendationResourceList | ForEach-Object {  
+  $Exclude = New-Object Amazon.TrustedAdvisor.Model.RecommendationResourceExclusion -Property @{Arn=$_.Arn;isExcluded=$True}  
+  Update-TAUpdateRecommendationResourceExclusionBatch -RecommendationResourceExclusion $Exclude  
+  Write-Output "Excluded: $($_.Arn)"
+}
