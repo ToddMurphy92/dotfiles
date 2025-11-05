06c44fe51667a1183f5445f088393f088a2db963
Todd Murphy <admin@toddmurphy.me>
2025-11-06 09:20:03 +1000

    Added ec2 fish function

---
+++ bin/.config/bin/ec2.fish
@@ -0,0 +1,6 @@
+#!/opt/homebrew/bin/fish
+function ec2s
+    aws ec2 describe-instances \
+        --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" \
+        --output table
+end
