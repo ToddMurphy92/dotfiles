38d1d5d1ef4d67d08de35702f53216ed05a300d9
Todd Murphy <admin@toddmurphy.me>
2025-11-27 18:05:51 +1000

    Fixed issue with auto-complete accepting on enter instead of right key

--- lazyvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ lazyvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,17 +1,20 @@
 -- Set nvim-cmp keybindings
 -- So that <Right> selects the current completion item
 return {
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      local cmp = require("cmp")
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Right>"] = cmp.mapping.confirm({ select = true }),
-        ["<CR>"] = function(fallback)
-          cmp.abort()
-          fallback()
-        end,
-      })
-    end,
-  },
+  print("Loading nvim-cmp keybindings plugin"),
+  "hrsh7th/nvim-cmp",
+  enabled = false,
+  print("Set nvim-cmp to disabled"),
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping = cmp.mapping({
+      print("Testing nvim-cmp keybindings"),
+      ["<Right>"] = cmp.mapping.confirm({ select = true }), -- Enter confirms completion
+      ["<Esc>"] = cmp.mapping.abort(), -- Escape aborts completion
+      ["<Tab>"] = cmp.mapping.select_next_item(), -- Tab selects next item
+      ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- Shift-Tab selects previous item
+      ["<C-u>"] = cmp.mapping.scroll_docs(-4), -- Ctrl-u scrolls docs up
+      ["<C-d>"] = cmp.mapping.scroll_docs(4), -- Ctrl-d scrolls docs down
+    })
+  end,
 }
