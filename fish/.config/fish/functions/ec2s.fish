736820bbf30ab2bbc356ddacfc007c13514b2bc7
Todd Murphy <admin@toddmurphy.me>
2026-06-11 14:10:28 +1000

    Added function to list EC2 instances

--- fish/.config/fish/functions/ec2s.fish
+++ fish/.config/fish/functions/ec2s.fish
@@ -1,5 +1,15 @@
+# Defined in /home/bryan/.config/fish/functions/ec2s.fish @ line 1
 function ec2s
     aws ec2 describe-instances \
-        --query "Reservations[].Instances[].[InstanceId,Tags[?Key=='Name'].Value|[0]]" \
+        --query "
+        sort_by(
+          Reservations[].Instances[].[
+            InstanceId,
+            PrivateIpAddress,
+            Tags[?Key=='Name'].Value | [0] || '',
+            Tags[?Key=='srg:function'].Value | [0] || ''
+          ],
+          &[2]
+        )" \
         --output table
 end
